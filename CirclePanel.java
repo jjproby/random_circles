2de75320b3d6bf6a4e22dbe1a8912c9befeb271c
jjproby <jjproby719@gmail.com>
2016-03-21 14:38:49 -0500

    trying stuff out with CirclePanel

--- CirclePanel.java
+++ CirclePanel.java
@@ -1,14 +1,17 @@
 
 package random_circles;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
 
 public class CirclePanel extends JPanel {
     
+    public static final Color BG_COLOR = new Color(180, 224, 248);
+    
     public CirclePanel() {
-        
+        this.setBackground(BG_COLOR);
     }
        
     @Override
