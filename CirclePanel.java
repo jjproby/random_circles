b6e1fc4f985ceabcda9cab6ed70409a2111a7399
jjproby <jjproby719@gmail.com>
2016-03-21 14:10:36 -0500

    Base Files

---
+++ CirclePanel.java
@@ -0,0 +1,19 @@
+
+package random_circles;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+
+public class CirclePanel extends JPanel {
+    
+    public CirclePanel() {
+        
+    }
+       
+    @Override
+    public void paintComponent( Graphics g ) {
+        
+    }
+    
+}
