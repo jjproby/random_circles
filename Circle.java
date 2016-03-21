fd280e2a751d41ded2f4a6f4805244b37120f78f
jjproby <jjproby719@gmail.com>
2016-03-21 14:10:17 -0500

    Base Files

---
+++ Circle.java
@@ -0,0 +1,33 @@
+
+package random_circles;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+
+
+public class Circle implements Colorable{
+    
+    public Circle( double x, double y, double radius) {
+        
+    }
+
+    @Override
+    public Color getColor() {
+        
+    }
+
+    @Override
+    public void setColor(Color c) {
+        
+    }
+    
+    public boolean intersects( Circle c ) {
+        
+    }
+    
+    public void draw( Graphics2D g2D, AffineTransform transform) {
+        
+    }
+    
+}
