dae5e0ac584c6f376f7f8d743671d5bcdfb48ec2
jjproby <jjproby719@gmail.com>
2016-03-21 14:38:25 -0500

    Started filling the intersects function

--- Circle.java
+++ Circle.java
@@ -13,8 +13,11 @@ public class Circle implements Colorable{
     double radius;
     private Color color;
     
-    public Circle( double x, double y, double radius) {
-        
+    public Circle( double x, double y, double radius, Color color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
     }
 
     @Override
@@ -28,7 +31,14 @@ public class Circle implements Colorable{
     }
     
     public boolean intersects( Circle c ) {
+        double distance = Math.sqrt(Math.pow(c.x - this.x,2) - Math.pow(c.y - this.y,2));
         
+        if (this.radius + c.radius < distance) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     
     public void draw( Graphics2D g2D, AffineTransform transform) {
