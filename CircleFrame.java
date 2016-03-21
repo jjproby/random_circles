ebf7807009f26be456a2eb8ed49fef1c0d3a5949
jjproby <jjproby719@gmail.com>
2016-03-21 14:09:31 -0500

    Base Files

---
+++ CircleFrame.java
@@ -0,0 +1,18 @@
+
+package random_circles;
+
+
+import javax.swing.JFrame;
+
+public class CircleFrame extends JFrame {
+
+    
+    public CircleFrame() {
+
+    } // Spitfire()
+    
+    public static void main(String[] args) {
+//fdsafd
+    } // main( String [] )
+    
+} // Spitfire
