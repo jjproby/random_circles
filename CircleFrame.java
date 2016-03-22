94869e04bfea119387e5f5ea5b1cfcf4b598724d
jjproby <jjproby719@gmail.com>
2016-03-21 19:17:47 -0500

    Adding code from Spitfire to CircleFrame

--- CircleFrame.java
+++ CircleFrame.java
@@ -2,17 +2,32 @@
 package random_circles;
 
 
+import java.awt.Container;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class CircleFrame extends JFrame {
 
+    public static final int FRAME_WIDTH = 768;
+    public static final int FRAME_HEIGHT = 768;
+    public static final String TITLE = "Random Circles";
     
     public CircleFrame() {
-
+        this.setSize( FRAME_WIDTH, FRAME_HEIGHT );
+        this.setTitle( TITLE );
+        this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        
+        Container pane = this.getContentPane();
+        CirclePanel panel = new CirclePanel();
+        Timer timer = new Timer( 40, panel );
+        timer.start();
+        pane.add( panel );
+        
+        this.setVisible( true  );
     } // Spitfire()
     
     public static void main(String[] args) {
-
+        CircleFrame circleFrame = new CircleFrame;
     } // main( String [] )
     
 } // Spitfire
